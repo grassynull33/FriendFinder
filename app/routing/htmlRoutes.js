3407d777c96f0ef519a3ff39a9d21e60df47f683
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-04-26 19:51:52 -0700

    add survey.html and content with submit button ajax call

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,8 +2,7 @@ var path = require("path");
 
 module.exports = function(app) {
 	app.get("/survey", function(req, res) {
-		res.send("survey");
-		// res.sendFile(path.join(__dirname, "/../public/survey.html"));
+		res.sendFile(path.join(__dirname, "/../public/survey.html"));
 	});
 
 	app.use(function(req, res) {
