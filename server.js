1bd9469719c0bf434d7a3613930c4d27bd2fa246
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-04-26 18:53:41 -0700

    set up server.js to listen

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
