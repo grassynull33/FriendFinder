397e5747d8bee67a8833141c56e3b992e6511a74
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-04-26 19:02:04 -0700

    add routing for home.html and add separate file for html routing

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
+
 app.listen(PORT, function() {
 	console.log("App listening on PORT: " + PORT);
 });
