cf1678fbef5cf5421348d703c5a6250663b3af9c
Yoon Lee <yoonlee@Yoons-MacBook-Air.local>
2017-04-28 21:19:08 -0700

    change port number to 8080

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
 // For serving of static CSS
 app.use(express.static(__dirname + "/app/css"));
